9c136ad53c05b0a457a1cdbb9318f809eb21ad1e
Tyler Grommesh <tgrommesh@icloud.com>
2021-06-19 17:18:17 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Tyler Grommesh on 6/19/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
